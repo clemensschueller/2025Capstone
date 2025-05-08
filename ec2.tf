dface42e0504c775f6889a067ff532f3661b5482
Clemens Schueller <schueller.cj@gmail.com>
2025-05-08 12:18:06 +0200

    Correct names of references

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,10 @@
 resource "aws_instance" "wordpress_primary" {
   ami                    = "ami-0d1bf5b68307103c2"  # Amazon Linux 2 in eu-central-1
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public[0].id  # Erstes Public Subnet (eu-central-1a)
+  subnet_id              = aws_subnet.public_1.id  # Erstes Public Subnet (eu-central-1a)
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.http.id
+    aws_security_group.ssh_sg.id,
+    aws_security_group.webserver_sg.id
   ]
   user_data              = file("userdata_wordpress.sh")  # Pfad zu deinem Script
 
@@ -17,5 +17,5 @@ resource "aws_instance" "wordpress_primary" {
 
 # Output the public IP
 output "public_ip" {
-  value = aws_instance.web_server.public_ip
+  value = aws_instance.wordpress_primary.public_ip
 }
