52ac3597cd214b1bc976fe39bff33f7ea4af6f11
Clemens Schueller <schueller.cj@gmail.com>
2025-05-08 17:22:45 +0200

    Update ami-id to right region

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,22 @@
 # EC2 Instance
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "wordpress_primary" {
-  ami                    = "ami-0d1bf5b68307103c2"  # Amazon Linux 2 in eu-central-1
+  ami                    = "ami-009082a6cd90ccd0e"  # Amazon Linux 2 in eu-central-1
   instance_type          = "t2.micro"
-  subnet_id              = module.vpc.public_subnet_ids[0]  # Erstes Public Subnet (eu-central-1a)
+  subnet_id              = module.vpc.public_subnet_ids[0]  
   vpc_security_group_ids = [
     aws_security_group.ssh_sg.id,
     aws_security_group.webserver_sg.id
   ]
-  user_data              = file("userdata_wordpress.sh")  # Pfad zu deinem Script
+  user_data              = file("userdata_wordpress.sh")  # Path to Script
 
   tags = {
     Name = "WordPress-Primary"
