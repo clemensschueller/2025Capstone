87a2258e50299258ec74bcaba8f2145a0c3a64d7
Clemens Schueller <schueller.cj@gmail.com>
2025-05-09 23:07:40 +0200

    Add key pair

--- ec2.tf
+++ ec2.tf
@@ -16,6 +16,8 @@ resource "aws_instance" "wordpress_primary" {
     aws_security_group.ssh_sg.id,
     aws_security_group.webserver_sg.id
   ]
+  associate_public_ip_address = true
+  key_name = "wordpress"
   user_data = file("userdata_wordpress.sh")  # Path to Script
 
   tags = {
