dff88fe2845d0fff03cd346ce8cd05370c66fac5
Clemens Schueller <schueller.cj@gmail.com>
2025-05-09 16:42:21 +0200

    Update ami to right region

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,22 @@
 # EC2 Instance
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
+#data "aws_ami" "amazon_linux_2" {
+# most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+#filter {
+#  name   = "name"
+#  values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+# }
+#}
 
 resource "aws_instance" "wordpress_primary" {
-  ami                    = "ami-009082a6cd90ccd0e"  # Amazon Linux 2 in eu-central-1
+  ami                    = "ami-085386e29e44dacd7"
   instance_type          = "t2.micro"
   subnet_id              = module.vpc.public_subnet_ids[0]  
   vpc_security_group_ids = [
     aws_security_group.ssh_sg.id,
     aws_security_group.webserver_sg.id
   ]
-  user_data              = file("userdata_wordpress.sh")  # Path to Script
+  user_data = file("userdata_wordpress.sh")  # Path to Script
 
   tags = {
     Name = "WordPress-Primary"
