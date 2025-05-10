0841012cdbe79241f5a149262fe859047051707f
Clemens Schueller <schueller.cj@gmail.com>
2025-05-11 01:27:24 +0200

    Add alb

---
+++ alb.tf
@@ -0,0 +1,15 @@
+# Application Load Balancer
+resource "aws_lb" "wordpress" {
+  name               = "wordpress-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "wordpress-alb"
+  }
+}
+
