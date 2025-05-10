04151c3869344be07bd5b91160dab9a81f5ed5a1
Clemens Schueller <schueller.cj@gmail.com>
2025-05-11 01:57:40 +0200

    Add alb

--- alb.tf
+++ alb.tf
@@ -13,3 +13,38 @@ resource "aws_lb" "wordpress" {
   }
 }
 
+# ALB Target Group
+resource "aws_lb_target_group" "wordpress" {
+  name     = "wordpress-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    enabled             = true
+    protocol            = "HTTP"
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    matcher             = "200-399"
+  }
+
+  tags = {
+    Name = "wordpress-target-group"
+  }
+}
+
+# ALB Listener
+resource "aws_lb_listener" "wp-listener" {
+  load_balancer_arn = aws_lb.wordpress.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress.arn
+  }
+}
