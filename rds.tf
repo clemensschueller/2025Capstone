55a4e59576a569d0b90e709b23f8be82e40f3070
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 19:16:17 +0200

    Add aws db instance

--- rds.tf
+++ rds.tf
@@ -8,3 +8,26 @@ resource "aws_db_subnet_group" "wordpress" {
   }
 }
 
+# RDS Instance
+resource "aws_db_instance" "wordpress" {
+  allocated_storage      = 10
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  identifier             = "wordpress-mysql"
+  db_name                = "wordpressdb"
+  username               = "admin"
+  password               = "Password123!" # In production, use aws_secretsmanager_secret or similar
+  parameter_group_name   = aws_db_parameter_group.wordpress.name
+  db_subnet_group_name   = aws_db_subnet_group.wordpress.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  multi_az               = false # Set to true for production
+  storage_encrypted      = false # Set true for better security in production
+
+  tags = {
+    Name = "wordpress-database"
+  }
+}
