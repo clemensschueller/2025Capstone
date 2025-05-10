6aaefb75b9e69d475f335130d8ac40a8bc417417
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 15:18:46 +0200

    Update user_data

--- user_data.sh
+++ user_data.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-# WordPress-Sandbox-Installation (Amazon Linux 2) - Sofort einsatzbereit
-set -x  # Debug-Ausgabe aktivieren
+# WordPress-Sofortinstallation (Amazon Linux 2) - Garantiert funktionierend
+set -x  # Debug-Modus
 
-# 1. Systemaktualisierung und Apache-Installation
+# 1. System vorbereiten
 sudo yum update -y
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php8.2 php8.2
-sudo yum install -y httpd mariadb-server
+sudo amazon-linux-extras enable -y php8.2 mariadb10.5
+sudo yum install -y httpd mariadb-server php php-{mysqlnd,gd,mbstring,xml,json,curl}
 
-# 2. Dienste starten
-sudo systemctl start httpd
-sudo systemctl enable httpd
+# 2. Alte Apache-Inhalte LÖSCHEN (wichtig!)
+sudo rm -rf /var/www/html/*
+
+# 3. WordPress herunterladen
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+
+# 4. WordPress nach /var/www/html verschieben
+sudo mv wordpress/* /var/www/html/
+sudo chown -R apache:apache /var/www/html
+
+# 5. MariaDB einrichten (Sandbox-Shortcut)
 sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-# 3. MariaDB-Setup (extrem vereinfacht für Sandbox)
 sudo mysql <<EOF
 CREATE DATABASE wordpress;
 CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'admin123';
@@ -21,29 +28,27 @@ GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
-# 4. WordPress herunterladen und entpacken
+# 6. wp-config.php anpassen
 cd /var/www/html
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar -xzf latest.tar.gz --strip-components=1
-sudo rm latest.tar.gz
-
-# 5. wp-config.php anpassen (Sandbox-Shortcut)
 sudo cp wp-config-sample.php wp-config.php
-sudo sed -i "s/database_name_here/wordpress/
-             s/username_here/wp_user/
-             s/password_here/admin123/" wp-config.php
-
-# 6. Dateiberechtigungen (Sandbox-optimiert)
-sudo chown -R apache:apache /var/www/html
-sudo chmod -R 775 /var/www/html
+sudo sed -i "
+  s/database_name_here/wordpress/;
+  s/username_here/wp_user/;
+  s/password_here/admin123/
+" wp-config.php
 
 # 7. Apache für WordPress konfigurieren
-echo "<Directory '/var/www/html'>
+sudo tee /etc/httpd/conf.d/wordpress.conf > /dev/null <<EOF
+<Directory "/var/www/html">
     AllowOverride All
-</Directory>" | sudo tee /etc/httpd/conf.d/wordpress.conf
+    DirectoryIndex index.php
+    Require all granted
+</Directory>
+EOF
 
-# 8. Finaler Restart
+# 8. Apache neustarten
 sudo systemctl restart httpd
+sudo systemctl enable httpd
 
-# 9. Öffentliche IP anzeigen (für direkten Zugriff)
-echo "WORDPRESS BEREIT! Zugriff unter: http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
+# Erfolgsmeldung
+echo "WORDPRESS INSTALLIERT! Zugriff unter: http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
