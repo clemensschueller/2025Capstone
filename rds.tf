315ae2c6a64091c6fc7d06acb72e8e44d3d8a075
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 21:10:41 +0200

    Fix vpc referencing

--- rds.tf
+++ rds.tf
@@ -1,7 +1,7 @@
 # RDS Subnet Group
 resource "aws_db_subnet_group" "wordpress" {
   name       = "wordpress-db-subnet-group"
-  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id] # Using both private subnets for high availability
+  subnet_ids = [module.vpc.aws_subnet.private_1.id, module.vpc.aws_subnet.private_2.id] # Using both private subnets for high availability
 
   tags = {
     Name = "WordPress DB Subnet Group"
