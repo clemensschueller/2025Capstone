fac05dfd692b66d5ea0866acc812396e7b973536
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 21:25:58 +0200

    Add igw_id to outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -9,3 +9,7 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 }
+
+output "igw_id" {
+  value = aws_internet_gateway.this.id
+}
