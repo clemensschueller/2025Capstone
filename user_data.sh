463c0894f68a50b676b0f42c524e70f37bd4c6ee
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 15:25:10 +0200

    Update user_data in order to configure wordpress

--- user_data.sh
+++ user_data.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-# WordPress-Sofortinstallation (Amazon Linux 2) - Garantiert funktionierend
 set -x  # Debug-Modus
 
-# 1. System vorbereiten
+# 1. Apache-Testseite LÖSCHEN (kritisch!)
+sudo rm -f /var/www/html/index.html
+
+# 2. Pakete installieren
 sudo yum update -y
 sudo amazon-linux-extras enable -y php8.2 mariadb10.5
-sudo yum install -y httpd mariadb-server php php-{mysqlnd,gd,mbstring,xml,json,curl}
-
-# 2. Alte Apache-Inhalte LÖSCHEN (wichtig!)
-sudo rm -rf /var/www/html/*
+sudo yum install -y httpd mariadb-server php php-{mysqlnd,gd,mbstring,xml}
 
 # 3. WordPress herunterladen
 cd /tmp
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
-
-# 4. WordPress nach /var/www/html verschieben
 sudo mv wordpress/* /var/www/html/
-sudo chown -R apache:apache /var/www/html
 
-# 5. MariaDB einrichten (Sandbox-Shortcut)
+# 4. Dateiberechtigungen setzen
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
+
+# 5. Datenbank einrichten (Sandbox-Modus)
 sudo systemctl start mariadb
 sudo mysql <<EOF
 CREATE DATABASE wordpress;
@@ -31,13 +31,9 @@ EOF
 # 6. wp-config.php anpassen
 cd /var/www/html
 sudo cp wp-config-sample.php wp-config.php
-sudo sed -i "
-  s/database_name_here/wordpress/;
-  s/username_here/wp_user/;
-  s/password_here/admin123/
-" wp-config.php
+sudo sed -i "s/database_name_here/wordpress/; s/username_here/wp_user/; s/password_here/admin123/" wp-config.php
 
-# 7. Apache für WordPress konfigurieren
+# 7. Apache KONFIGURIEREN (wichtig!)
 sudo tee /etc/httpd/conf.d/wordpress.conf > /dev/null <<EOF
 <Directory "/var/www/html">
     AllowOverride All
@@ -46,9 +42,8 @@ sudo tee /etc/httpd/conf.d/wordpress.conf > /dev/null <<EOF
 </Directory>
 EOF
 
-# 8. Apache neustarten
+# 8. Apache NEUSTARTEN (absolut kritisch)
 sudo systemctl restart httpd
-sudo systemctl enable httpd
 
 # Erfolgsmeldung
-echo "WORDPRESS INSTALLIERT! Zugriff unter: http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
+echo "WORDPRESS LÄUFT! Zugriff unter: http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
