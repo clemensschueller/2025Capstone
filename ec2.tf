e92893258b88fa3ba708edc12751c085be39bccf
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 02:45:23 +0200

    Update key_name to vockey

--- ec2.tf
+++ ec2.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "wordpress_primary" {
     aws_security_group.webserver_sg.id
   ]
   associate_public_ip_address = true
-  key_name = "wordpress"
+  key_name = "vockey"
   user_data = file("userdata_wordpress.sh")  # Path to Script
 
   tags = {
