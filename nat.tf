f7b34b390825fe5e3159ee7076687aab5e3ea8c7
Clemens Schueller <schueller.cj@gmail.com>
2025-05-10 20:55:13 +0200

    Add resources to nat.tf

---
+++ nat.tf
@@ -0,0 +1,30 @@
+# NAT Gateway Elastic IP
+resource "aws_eip" "nat" {
+  count  = var.create_nat_gateway ? 1 : 0
+  domain = "vpc"
+
+  tags = {
+    Name = "wordpress-nat-eip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  count         = var.create_nat_gateway ? 1 : 0
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public_1.id # Place in same AZ as Wordpress EC2 instance
+
+  tags = {
+    Name = "wordpress-nat-gateway"
+  }
+
+  # To ensure proper ordering
+  depends_on = [aws_internet_gateway.main]
+}
+
+# Update Private Route Table to route through NAT Gateway
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+}
