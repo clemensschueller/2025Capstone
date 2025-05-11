f185ccb4910e0ed6bb86c6ed10a1785d9436dbfb
Clemens Schueller <schueller.cj@gmail.com>
2025-05-11 15:02:29 +0200

    Add auto scaling group for asg

--- asg.tf
+++ asg.tf
@@ -32,3 +32,29 @@ resource "aws_launch_template" "wordpress" {
     Name = "wordpress-launch-template"
   }
 }
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "wp-autoscaling-group" {
+  name                = "wordpress-asg"
+  desired_capacity    = 2
+  min_size            = 1
+  max_size            = 4
+  vpc_zone_identifier = module.vpc.public_subnet_ids
+  target_group_arns   = [aws_lb_target_group.wordpress.arn]
+  health_check_type   = "ELB"
+  default_cooldown    = 30 # Added for faster scaling during testing
+
+  launch_template {
+    id      = aws_launch_template.wordpress.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "wordpress-asg-instance"
+    propagate_at_launch = true
+  }
+
+  # Wait for capacity to be available
+  wait_for_capacity_timeout = "10m"
+}
