016b6c0e216f0837c2c7941b877ef0921296eaf4
Clemens Schueller <schueller.cj@gmail.com>
2025-05-11 14:28:41 +0200

    Change to dynamic ami search

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,18 @@
 # EC2 Instance
-#data "aws_ami" "amazon_linux_2" {
-# most_recent = true
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#filter {
-#  name   = "name"
-#  values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-# }
-#}
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
 
+# Primary WordPress Instance
+# hardcode ami ami-085386e29e44dacd7
 resource "aws_instance" "wordpress_primary" {
-  ami           = "ami-085386e29e44dacd7"
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.ec2_instance_type_t2micro
   subnet_id     = module.vpc.public_subnet_ids[0]
   vpc_security_group_ids = [
