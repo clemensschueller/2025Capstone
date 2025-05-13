7e892bd36a43ac64c50938b3ca151dfec79f851a
Clemens Schueller <schueller.cj@gmail.com>
2025-05-13 23:06:33 +0200

    Comment out unused referencing

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,17 +3,18 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  value = [aws_subnet.public_1.id]
 }
+#add aws_subnet.public_2.id if you want to add another public subnet
 
-output "private_subnet_ids" {
-  value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-}
+# output "private_subnet_ids" {
+#   value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+# }
 
 output "igw_id" {
   value = aws_internet_gateway.this.id
 }
 
-output "private_route_table_id" {
-  value = aws_route_table.private.id
-}
+# output "private_route_table_id" {
+#   value = aws_route_table.private.id
+# }
