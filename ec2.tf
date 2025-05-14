1e43028e03c086c8c17e1da921b3fbf35a24ebcc
Clemens Schueller <schueller.cj@gmail.com>
2025-05-14 09:04:42 +0200

    Update ami id

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@
 # }
 
 # Primary WordPress Instance
-# hardcode ami ami-0c0ba4e76e4392ce9
+# hardcode ami "ami-04999cd8f2624f834"
 resource "aws_instance" "wordpress_primary" {
   ami           = "ami-04999cd8f2624f834"
   instance_type = var.ec2_instance_type_t2micro
